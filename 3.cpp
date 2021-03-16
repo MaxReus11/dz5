e1ff1a6c65495c2407d746c21142110cf9e4545a
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-17 02:17:58 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
-
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::string line;
-	std::cout<<"Enter the string to find domains:"<<std::endl;
+    std::string line = "а а а3@ffs.fef.fef fgf.rr@gtf45.fgr ggf@ya.ru ggf@ya.ya.ru";
+	//std::cout<<"Enter the string to find domains:"<<std::endl;
 	std::regex pattern(R"((([[:alpha:]]|[[:alnum:],._]){3,}[@])(([[:alpha:]]+[.]){1,}[[:alpha:]]{2,6}))");
-	getline(std::cin, line);
+	//getline(std::cin, line);
 	std::sregex_iterator begin(line.cbegin(), line.cend(), pattern);
 	std::sregex_iterator end;
+	std::cout<<"There are some domains:"<<std::endl;
 	std::for_each(begin, end, [](const std::smatch& m)
 		{
 			std::cout << m[3] << std::endl;
