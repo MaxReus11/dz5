b25dcb077e305ccbee8f4e4495d314b535a55215
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-17 01:47:19 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -8,14 +8,14 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     std::string line;
-	std::regex pattern(R"(([[:alpha:]][[:alnum:],._]{3,}[@])([[:alpha:]]+[.]?[[:alpha:]]+[.]?[[:alpha:]]+[.]?[[:alpha:]]+[.][[:alpha:]]{2,6}))");
+	std::regex pattern(R"((([[:alpha:]]|[[:alnum:],._]){3,}[@])(([[:alpha:]]+[.]){1,}[[:alpha:]]{2,6}))");
 	getline(std::cin, line);
 	std::sregex_iterator begin(line.cbegin(), line.cend(), pattern);
 	std::sregex_iterator end;
 
 	std::for_each(begin, end, [](const std::smatch& m)
 		{
-			std::cout << m[2] << std::endl;
+			std::cout << m[3] << std::endl;
 		});
 	return 0;
 }
