93d8463234b8a6d2227860aabe1099557a31d806
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-17 02:38:00 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
+#include <sstream>
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::string line;
-	std::cout<<"Enter the string to find date and time:"<<std::endl;
+	//std::cout<<"Enter the string to find date and time:"<<std::endl;
     std::regex pattern1(R"(([0-9]{4}[.]([0][0-9]|[1][012])[.]([012][0-9]|[3][01]))|(([012][0-9]|[3][01])[.]([0][0-9]|[1][012])[.][0-9]{4}))");
 	std::regex pattern2(R"(([01][0-9]|[2][0123])[:][0-5][0-9][:][0-5][0-9])");
-
-	for (std::string line; getline(std::cin, line);) {
+	 std::istringstream input;
+	 input.str("12.02.2002 32.54.1000 23:32:54 24:21:43");
+	 std::cout<<"The string to find date and time: "<<input.str()<<std::endl;
+	for (std::string line; getline(input, line);) {
 		std::sregex_iterator begin1(line.cbegin(), line.cend(), pattern1);
 		std::sregex_iterator end1;
 
