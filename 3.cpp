3ff4e0a6ca9d276ba934fe2af101b757f0b10c16
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-17 01:51:12 +0300

    Create 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -8,11 +8,11 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     std::string line;
+	std::cout<<"Enter the string to find domains:"<<std::endl;
 	std::regex pattern(R"((([[:alpha:]]|[[:alnum:],._]){3,}[@])(([[:alpha:]]+[.]){1,}[[:alpha:]]{2,6}))");
 	getline(std::cin, line);
 	std::sregex_iterator begin(line.cbegin(), line.cend(), pattern);
 	std::sregex_iterator end;
-
 	std::for_each(begin, end, [](const std::smatch& m)
 		{
 			std::cout << m[3] << std::endl;
