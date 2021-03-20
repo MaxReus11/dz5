dbb2e4cd1ffaca701d17fdeb756b05e73c067dd5
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-20 17:46:24 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,16 +6,19 @@
 #include <iterator>
 
 int main() {
+    double RoublesToEU = 88.0;
+    double RoublesToUSD = 74.0;
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
     std::cin.imbue(std::locale("deu_DEU.UTF-8"));
-    long double money; 
+    long double money;
+    std::cout << "Enter the amount of roubles you want to convert: ";
     std::cin >> std::get_money(money);
     std::cout.imbue(std::locale("rus_rus.UTF-8"));
     std::cout << "ru: " << std::showbase << std::put_money(money) << std::endl;
     std::cout.imbue(std::locale("deu_DEU.UTF-8"));
-    std::cout << "deu: "  << std::put_money(money/88) << std::endl;
+    std::cout << "deu: " << std::put_money(money / RoublesToEU) << std::endl;
     std::cout.imbue(std::locale("en_us.UTF-8"));
-    std::cout << "us: " << std::put_money(money/74) << std::endl;
+    std::cout << "us: " << std::put_money(money / RoublesToUSD) << std::endl;
 
 }
